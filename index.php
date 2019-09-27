ddc647c905b3cf810e21c859028a789436090809
Albert Alay-ay <alayaysantos@gmail.com>
2019-09-27 21:04:38 +0800

    added files, modified index.php&index.css

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
-<html>
-    <head>
-        <title>Sanse Cafe</title>
-    </head>
-    <body>
-        <h1>Hello</h1>
-        <script src = "app.js"></script>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <title>Sanse Cafe -Serving-</title>
+</head>
+<body>
+    <div class="header"><img src="img/icon.svg" alt="coffe-bean" id="icon"><span id="nav-label"> Sanse Cafe</span></div>
+    <div class = "intro">
+        <div class="milktea">
+            <img src="img/milktea.svg" alt="milktea">
+        </div>
+        <div class="motto">
+            <h1 id="motto-text">Do It Better Than Your Best Yesterday</h1>
+        </div>
+        <div class="login"></div>
+    </div>
+</body>
 </html>
