a90f56db4c4ecbe7e5ced82e100fb2c75040f488
Albert Alay-ay <alayaysantos@gmail.com>
2019-09-28 22:23:40 +0800

    modified index.php and index.css

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="index.css">
-    <title>Sanse Cafe</title>
+    <title>Camp Boomy Cafe</title>
 </head>
 <body>
-    <div class="header"><img src="img/icon.svg" alt="coffe-bean" id="icon"><span id="nav-label"> Sanse Cafe</span></div>
+    <div class="header"><img src="img/icon.svg" alt="coffe-bean" id="icon"><span id="nav-label"> Camp Boomy Cafe</span></div>
     <div class = "intro">
         <div class="motto">
             <h1 id="motto-text">Do It Better Than Your Best Yesterday</h1>
@@ -16,8 +16,17 @@
         <div class="milktea">
             <img src="img/milktea.svg" alt="milktea">
         </div>
-        <div class="login">
-            
+        <div class="signin">
+            <form>
+                <div class="input">
+                    <label>Username</label>
+                    <input type="text" name="uname">
+                </div>
+                <div class="input">
+                    <label>Password</label>
+                    <input type="password" name="pw">
+                </div>
+            </form>
         </div>
     </div>
 </body>
