dcfe8ae2d26cddf9fd982e7c31de67156f8c90f3
Albert Alay-ay <alayaysantos@gmail.com>
2019-09-30 13:41:15 +0800

    updated index

--- index.php
+++ index.php
@@ -25,5 +25,29 @@
             </form>
         </div>
     </div>
+    <div class="main">
+            <div class="box">
+                <div class="left">
+                    <img src="img/scroll.svg" alt="scroll">
+                    <span class="policyheader">Mission</span>
+                    <p class="policytext">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae ad accusamus atque ab necessitatibus iusto saepe cupiditate fugit doloremque sed quibusdam nisi quasi eum at repellat amet, recusandae a.
+                        Minima nam aliquam quibusdam id harum nisi, quam rerum perferendis eos officia dolorem. In et aut fugit nulla, delectus accusamus quasi mollitia eos quam reprehenderit dicta cupiditate blanditiis quod neque.
+                        Eligendi, quam velit beatae ipsam cum voluptatum reiciendis in iusto nisi architecto error harum culpa labore repellendus voluptatibus asperiores modi ratione odit vero eaque neque hic officia odio aliquam. Autem?
+                        Eveniet ipsum neque quisquam nostrum. Incidunt odit aspernatur dolorum, eligendi tempore sed ea omnis velit provident autem sunt, doloremque quia est vitae explicabo necessitatibus impedit voluptatum. Perspiciatis dolor earum quis.
+                        Nobis blanditiis minus, obcaecati debitis facere ipsa cupiditate earum, quas laborum sequi doloribus rerum fugit dolorum deserunt nulla, quis maxime voluptatum tempore animi atque consequuntur similique non libero totam. Aperiam.
+                    </p>
+                </div>
+                <div class="right">
+                    <img src="img/torch.svg" alt="torch">
+                    <span class ="policyheader">Vision</span>
+                    <p class="policytext">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatibus quasi id, saepe at non, illo vero perferendis aspernatur expedita distinctio amet architecto laudantium maiores. Debitis mollitia velit et porro.
+                        Ab modi itaque dignissimos eligendi dicta voluptas doloremque doloribus expedita quidem repudiandae molestiae ex odio delectus hic alias nostrum nulla, labore, maxime ducimus cum. Sed, dolores. Quis dolorem quidem veniam.
+                        At ut nemo labore. Officia, animi mollitia? Corporis recusandae odit unde voluptas quas vel dicta commodi at magnam. Ipsa error ea quis in minus molestias corrupti aliquid doloremque ex sapiente!
+                    </p>
+                </div>
+            </div>
+    </div>
 </body>
 </html>
