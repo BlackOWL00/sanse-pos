a9079c77074e353e3b286e0c8aa3f62b237cf193
Albert Alay-ay <alayaysantos@gmail.com>
2019-10-15 17:56:27 +0800

    Remove moments, simplified Chart

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hello Hello World");
+function fnOpenNav(){
+    document.getElementById("mySideNav").style.width = "335px";
+
+}
+
+function fnCloseNav(){
+    document.getElementById("mySideNav").style.width = "0px";
+}
+
+var ctx = document.getElementById('lineGraph');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        datasets: [{
+            label: 'Sales of the Week',
+            data: [12, 19, 3, 5, 2, 3, 20],
+            backgroundColor: [
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 3
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="dashboard.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Dashboard</title>
 </head>
@@ -30,21 +31,17 @@
         </a>
 </div>
 <div class="main">
+    <div class="burger">
     <span id = "sidebar-btn" onclick = "fnOpenNav()">&#9776;</span>
+    </div>
     <div class="graph1">
         <canvas id="lineGraph"></canvas>
     </div>
+    <div class="graph2">
+        <canvas id="donutGraph"></canvas>
+        <canvas id="barGraph"></canvas>
+    </div>
 </div>
 </body>
-<script>
-    function fnOpenNav(){
-        document.getElementById("mySideNav").style.width = "335px";
-
-    }
-
-    function fnCloseNav(){
-        document.getElementById("mySideNav").style.width = "0px";
-    }
-</script>
-<script src=""></script>
-</html>
+<script src = "app.js"></script>
+</html> 
