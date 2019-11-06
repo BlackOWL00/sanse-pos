5639e0d34c2c7a542d04f27ca49c932880c844f1
Albert Alay-ay <alayaysantos@gmail.com>
2019-11-06 11:11:20 +0800

    Added the sidebar and header in pos.js and pos.css

---
+++ pos.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="dashboard.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>At Your Service</title>
+</head>
+<body>
+<div id ="mySideNav" class="sidebar">
+        <div class="sidebarlogo">
+            <img src="img/boomy.png" alt="BOOMY" id = "circleimg">
+            <h1>Camp Boomy Cafe</h1>
+        </div>
+        <ul class="list-group">
+            <a href="dashboard.php"><li class="list-group-item">Dashboard</li></a>
+            <a href="#"><li class="list-group-item">Point of Sales</li></a>
+            <a href="#"><li class="list-group-item">Items</li></a>
+            <a href="#"><li class="list-group-item">Records</li></a>
+            <a href="#"><li class="list-group-item">Settings</li></a>
+            <a href="javascript:void(0)" class="closebtn" onclick="fnCloseNav()">&times;</a>
+        </ul>
+</div>
+<div class="header">
+        <a href="dashboard.php">
+        <img src="img/boomy.png" alt="Boomy" id="icon">
+        <span> Camp Boomy Cafe</span>
+        </a>
+</div>
+<div class="main">
+    <div class="burger">
+    <span id = "sidebar-btn" onclick = "fnOpenNav()">&#9776;</span>
+    </div>
+</div>
+<script>
+function fnOpenNav(){
+    document.getElementById("mySideNav").style.width = "335px";
+
+}
+
+function fnCloseNav(){
+    document.getElementById("mySideNav").style.width = "0px";
+}
+</script>
+</body>
+</html>
