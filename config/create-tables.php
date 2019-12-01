c25d578e2f713aa462eaa441be1ba41347d431c5
Albert Alay-ay <alayaysantos@gmail.com>
2019-12-01 18:31:14 +0800

    added auto create db and tbl in sql

--- config/create-tables.php
+++ config/create-tables.php
@@ -1,5 +1,6 @@
 <?php
 function fn_create_tables(mysqli $conn){
+	//Accounts table
     $tbl_accounts = "SELECT * FROM tbl_accounts";
 	$check_tbl_accounts = mysqli_query($conn, $tbl_accounts);
 	if(!$check_tbl_accounts){
@@ -13,6 +14,47 @@ function fn_create_tables(mysqli $conn){
 		PRIMARY KEY(personal_id)
 	)";
 	mysqli_query($conn, $create_tbl_accounts);
-    }
+	}
+	//RECORDS TABLE
+	$tbl_records = "SELECT * FROM tbl_records";
+	$check_tbl_records = mysqli_query($conn, $tbl_records);
+	if(!$check_tbl_records){
+		$create_tbl_records = "CREATE TABLE tbl_records(
+		order_id INT AUTO_INCREMENT,
+		date_time TIMESTAMP default NOW(),
+		ordered_item VARCHAR(255) NOT NULL,
+		order_qty INT NOT NULL,
+		transac_code VARCHAR(255) NOT NULL,
+		PRIMARY KEY(order_id)
+		)";
+	mysqli_query($conn, $create_tbl_records);
+	}
+	//transactions table
+	$tbl_transactions = "SELECT * FROM tbl_transactions";
+	$check_tbl_transactions = mysqli_query($conn, $tbl_transactions);
+	if(!$check_tbl_transactions){
+		$create_tbl_transactions = "CREATE TABLE tbl_transactions(
+		transac_id INT AUTO_INCREMENT,
+		transac_code VARCHAR(255) NOT NULL,
+		date_time TIMESTAMP default NOW(),
+		transac_total DECIMAL(6, 2),
+		PRIMARY KEY(transac_id)
+		)";
+	mysqli_query($conn, $create_tbl_transactions);
+	}
+	//ITEMS
+	$tbl_items = "SELECT * FROM tbl_items";
+	$check_tbl_items = mysqli_query($conn, $tbl_items);
+	if(!$check_tbl_items){
+		$create_tbl_items = "CREATE TABLE tbl_items(
+		items_id INT AUTO_INCREMENT,
+		items_name VARCHAR(255) NOT NULL,
+		items_price DECIMAL(3, 2),
+		items_category VARCHAR(255) NOT NULL
+		PRIMARY KEY(items_id)
+		)";
+	mysqli_query($conn, $create_tbl_items);
+	}
+
 }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require "config/config.php";
-require "config/db.php";
+require ("config/config.php");
+require ("config/db.php");
 
 ?>
 <!DOCTYPE html>
